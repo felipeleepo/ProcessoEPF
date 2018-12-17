1bc0201c5af15be7d2c8210958b47a27bc5530f4
Felipe Barbosa <felipebinfo@gmail.com>
2018-12-17 14:22:51 -0300

    Add files via upload

---
+++ scripts/processElementData.js
@@ -0,0 +1,80 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_kymxgfKsEeifb8aqeDERtgActivity"]="./../../Felipe/deliveryprocesses/resources/Processo do Felipe_3AC135A1_3ac135a1_Activity.jpeg"
+contentPage.processPage.imageFiles["_kymxgfKsEeifb8aqeDERtg,_c5c-IfMTEeiD__tzBoewKAActivity"]="./../../Felipe/deliveryprocesses/resources/Análise de Problema_84E12DF5_3f385eea_Activity.jpeg"
+contentPage.processPage.imageFiles["_kymxgfKsEeifb8aqeDERtg,_c5c-IfMTEeiD__tzBoewKA,_kWbtEfMTEeiD__tzBoewKAActivity"]="./../../Felipe/deliveryprocesses/resources/Reunião_75B75BBB_6da0aaf9_Activity.jpeg"
+contentPage.processPage.imageFiles["_kymxgfKsEeifb8aqeDERtg,_AqFv8fMUEeiD__tzBoewKAActivity"]="./../../Felipe/deliveryprocesses/resources/Revisão_4F63EC8B_9bb1d80_Activity.jpeg"
+contentPage.processPage.imageFiles["_kymxgfKsEeifb8aqeDERtg,_89BoUfMTEeiD__tzBoewKAActivity"]="./../../Felipe/deliveryprocesses/resources/Implementação_7FB5D63B_3a0d0730_Activity.jpeg"
+contentPage.processPage.imageFiles["_kymxgfKsEeifb8aqeDERtg,_2qi9ofMTEeiD__tzBoewKAActivity"]="./../../Felipe/deliveryprocesses/resources/Análise de Solução_F8912BA2_b2e85c97_Activity.jpeg"
+contentPage.processPage.elementUrls["_99dxofMTEeiD__tzBoewKA"] = ["Desenvolvimento","Felipe/deliveryprocesses/Desenvolvimento_FB887067.html"];
+contentPage.processPage.elementUrls["_NA1DIfMUEeiD__tzBoewKA"] = ["Distribuição","Felipe/deliveryprocesses/Distribuição_A9A2B500.html"];
+contentPage.processPage.elementUrls["_kWbtEfMTEeiD__tzBoewKA"] = ["Reunião","Felipe/deliveryprocesses/Reunião_75B75BBB.html"];
+contentPage.processPage.elementUrls["_dAtOkP75Eei2nYopsMT9Zw"] = ["Teste de Perfomance","Felipe/tasks/Teste de Perfomance_4D6F3775.html"];
+contentPage.processPage.elementUrls["__6XKgf74Eei2nYopsMT9Zw"] = ["Histórias de Usuário","Felipe/workproducts/Histórias de Usuário_B8D03ECB.html"];
+contentPage.processPage.elementUrls["__F9qwfMTEeiD__tzBoewKA"] = ["Testes","Felipe/deliveryprocesses/Testes_33DDFE4A.html"];
+contentPage.processPage.elementUrls["_7_rPQfMTEeiD__tzBoewKA"] = ["Exposição","Felipe/deliveryprocesses/Exposição_4C535FB7.html"];
+contentPage.processPage.elementUrls["_5zwU0PMUEeiD__tzBoewKA"] = ["planejar_protipapacao","Felipe/tasks/Planejar Protipapação_FBA6C784.html"];
+contentPage.processPage.elementUrls["_kymxgfKsEeifb8aqeDERtg"] = ["processo felipe","Felipe/deliveryprocesses/Processo do Felipe_3AC135A1.html"];
+contentPage.processPage.elementUrls["_0QOa1fMUEeiD__tzBoewKA"] = ["desenvolvedor","Felipe/roles/desenvolvedor_7D60B7FF.html"];
+contentPage.processPage.elementUrls["_LSYoIP74Eei2nYopsMT9Zw"] = ["Propor Ambientação","Felipe/tasks/Propor Ambientação_96F82684.html"];
+contentPage.processPage.elementUrls["_6yUrUfMTEeiD__tzBoewKA"] = ["Planejamento","Felipe/deliveryprocesses/Planejamento_32E9CF8D.html"];
+contentPage.processPage.elementUrls["_dgC18P75Eei2nYopsMT9Zw"] = ["Implementar Histórias de Usuário","Felipe/tasks/Implementar Histórias de Usuário_CF5B8026.html"];
+contentPage.processPage.elementUrls["_0QOa0PMUEeiD__tzBoewKA"] = ["brainstorm","Felipe/tasks/Brainstorm_E35167AF.html"];
+contentPage.processPage.elementUrls["_50o7EfMTEeiD__tzBoewKA"] = ["Ambiente","Felipe/deliveryprocesses/Ambiente_EBD32F60.html"];
+contentPage.processPage.elementUrls["_lZZ-4P75Eei2nYopsMT9Zw"] = ["Exposição - Implementação","Felipe/tasks/Exposição - Implementação_BA9D0A5A.html"];
+contentPage.processPage.elementUrls["_q9FRwP8YEeiae9UfREJxQA"] = ["Revisar Documentação","Felipe/tasks/Revisar Documentação_B4DB1CF3.html"];
+contentPage.processPage.elementUrls["_lZZ-5f75Eei2nYopsMT9Zw"] = ["desenvolvedor","Felipe/roles/desenvolvedor_7D60B7FF.html"];
+contentPage.processPage.elementUrls["_lZZ-4f75Eei2nYopsMT9Zw"] = ["interessados","Felipe/roles/interessados_1461DA35.html"];
+contentPage.processPage.elementUrls["_OvflNP8ZEeiae9UfREJxQA"] = ["analista","Felipe/roles/analista_77DAE932.html"];
+contentPage.processPage.elementUrls["__1MIQfMTEeiD__tzBoewKA"] = ["Exposição","Felipe/deliveryprocesses/Exposição_F3AFC35F.html"];
+contentPage.processPage.elementUrls["_CXuhAPMVEeiD__tzBoewKA"] = ["escolher_arquitetura","Felipe/tasks/Escolher Arquitetura_AB35BE40.html"];
+contentPage.processPage.elementUrls["_1o_aAfMTEeiD__tzBoewKA"] = ["Exposição","Felipe/deliveryprocesses/Exposição_1CEFCAE1.html"];
+contentPage.processPage.elementUrls["_clRPwP75Eei2nYopsMT9Zw"] = ["Teste de código","Felipe/tasks/Teste de Código_F9028586.html"];
+contentPage.processPage.elementUrls["_AqFv8fMUEeiD__tzBoewKA"] = ["Revisão","Felipe/deliveryprocesses/Revisão_4F63EC8B.html"];
+contentPage.processPage.elementUrls["_c5c-IfMTEeiD__tzBoewKA"] = ["Análise de Problema","Felipe/deliveryprocesses/Análise de Problema_84E12DF5.html"];
+contentPage.processPage.elementUrls["_6nA0AP8YEeiae9UfREJxQA"] = ["Preparar Solução","Felipe/tasks/Preparar Solução_17361AB7.html"];
+contentPage.processPage.elementUrls["_4D7LYfMTEeiD__tzBoewKA"] = ["Arquitetura","Felipe/deliveryprocesses/Arquitetura_F0396D4E.html"];
+contentPage.processPage.elementUrls["_Ku_kMP8ZEeiae9UfREJxQA"] = ["Exposição - Revisão","Felipe/tasks/Exposição - Revisão_4AD0D71B.html"];
+contentPage.processPage.elementUrls["_OvWbRP8ZEeiae9UfREJxQA"] = ["desenvolvedor","Felipe/roles/desenvolvedor_7D60B7FF.html"];
+contentPage.processPage.elementUrls["_AlXLgPMVEeiD__tzBoewKA"] = ["propor arquitetura","Felipe/tasks/Propor Arquitetura_C13A39C8.html"];
+contentPage.processPage.elementUrls["_-pO2CfMUEeiD__tzBoewKA"] = ["contrato_de_servico","Felipe/workproducts/Contrato de Serviço_DC511579.html"];
+contentPage.processPage.elementUrls["_MQRMsP74Eei2nYopsMT9Zw"] = ["Escolher Ambiente","Felipe/tasks/Escolher Ambiente_73EF0E73.html"];
+contentPage.processPage.elementUrls["_u8TG4P74Eei2nYopsMT9Zw"] = ["Exposição - Solução","Felipe/tasks/Exposição - Solução_A34BF89.html"];
+contentPage.processPage.elementUrls["_OvWbR_8ZEeiae9UfREJxQA"] = ["interessados","Felipe/roles/interessados_1461DA35.html"];
+contentPage.processPage.elementUrls["_-pO2A_MUEeiD__tzBoewKA"] = ["desenvolvedor","Felipe/roles/desenvolvedor_7D60B7FF.html"];
+contentPage.processPage.elementUrls["_Da8RgPMVEeiD__tzBoewKA"] = ["documento de arquitetura","Felipe/tasks/Desenvolver Documento de Arquitetura_14C6EAA4.html"];
+contentPage.processPage.elementUrls["_0QOa0fMUEeiD__tzBoewKA"] = ["interessados","Felipe/roles/interessados_1461DA35.html"];
+contentPage.processPage.elementUrls["_0QOa0_MUEeiD__tzBoewKA"] = ["analista","Felipe/roles/analista_77DAE932.html"];
+contentPage.processPage.elementUrls["_-pO2BfMUEeiD__tzBoewKA"] = ["interessados","Felipe/roles/interessados_1461DA35.html"];
+contentPage.processPage.elementUrls["_0Y_KsfMTEeiD__tzBoewKA"] = ["Prototipação","Felipe/deliveryprocesses/Prototipação_391EBAA4.html"];
+contentPage.processPage.elementUrls["_2qi9ofMTEeiD__tzBoewKA"] = ["Análise de Solução","Felipe/deliveryprocesses/Análise de Solução_F8912BA2.html"];
+contentPage.processPage.elementUrls["_b2_NcP75Eei2nYopsMT9Zw"] = ["Teste de Conectividade","Felipe/tasks/Teste de Conectividade_50DC7034.html"];
+contentPage.processPage.elementUrls["_LSYoIf74Eei2nYopsMT9Zw"] = ["analista","Felipe/roles/analista_77DAE932.html"];
+contentPage.processPage.elementUrls["_JwGSMfMUEeiD__tzBoewKA"] = ["Documentação","Felipe/deliveryprocesses/Documentação_3EFB42AF.html"];
+contentPage.processPage.elementUrls["_-pO2B_MUEeiD__tzBoewKA"] = ["prototipo","Felipe/workproducts/Protótipo_A028F872.html"];
+contentPage.processPage.elementUrls["_89BoUfMTEeiD__tzBoewKA"] = ["Implementação","Felipe/deliveryprocesses/Implementação_7FB5D63B.html"];
+contentPage.processPage.elementUrls["_OvWbQf8ZEeiae9UfREJxQA"] = ["analista","Felipe/roles/analista_77DAE932.html"];
+contentPage.processPage.elementUrls["_3Fk3UfMUEeiD__tzBoewKA"] = ["contrato_de_servico","Felipe/workproducts/Contrato de Serviço_DC511579.html"];
+contentPage.processPage.elementUrls["_MM6S4fMUEeiD__tzBoewKA"] = ["Exposição","Felipe/deliveryprocesses/Exposição_2D75DA0C.html"];
+contentPage.processPage.elementUrls["_lZZ-4_75Eei2nYopsMT9Zw"] = ["analista","Felipe/roles/analista_77DAE932.html"];
+contentPage.processPage.elementUrls["_AlXLg_MVEeiD__tzBoewKA"] = ["desenvolvedor","Felipe/roles/desenvolvedor_7D60B7FF.html"];
+contentPage.processPage.elementUrls["_MQRMsf74Eei2nYopsMT9Zw"] = ["ambiente","Felipe/workproducts/Ambiente_E9870DA2.html"];
+contentPage.processPage.elementUrls["_LSYoI_74Eei2nYopsMT9Zw"] = ["desenvolvedor","Felipe/roles/desenvolvedor_7D60B7FF.html"];
+contentPage.processPage.elementUrls["_AlXLgfMVEeiD__tzBoewKA"] = ["analista","Felipe/roles/analista_77DAE932.html"];
+contentPage.processPage.elementUrls["_3Fk3UPMUEeiD__tzBoewKA"] = ["reuniao contratual","Felipe/tasks/Reunião Contratual_1C79FA9D.html"];
+contentPage.processPage.elementUrls["_7tdocPMUEeiD__tzBoewKA"] = ["realizar_prototipacao","Felipe/tasks/Realizar Prototipação_B2C82C83.html"];
+contentPage.processPage.elementUrls["_eA-1QP74Eei2nYopsMT9Zw"] = ["Reunião de Planejamento","Felipe/tasks/Reunião de Planejamento_8DDE7056.html"];
+contentPage.processPage.elementUrls["_-pO2APMUEeiD__tzBoewKA"] = ["exposicao_analise","Felipe/tasks/Exposição - Análise_5673F1F5.html"];
+contentPage.processPage.elementUrls["_-pO2AfMUEeiD__tzBoewKA"] = ["analista","Felipe/roles/analista_77DAE932.html"];
+contentPage.processPage.elementUrls["_OvflN_8ZEeiae9UfREJxQA"] = ["desenvolvedor","Felipe/roles/desenvolvedor_7D60B7FF.html"];
